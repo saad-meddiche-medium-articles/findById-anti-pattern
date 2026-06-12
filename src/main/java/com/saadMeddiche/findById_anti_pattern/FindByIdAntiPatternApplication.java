f17782106ec7057846571bf8e7c1529cbed08469
SaadMeddiche <saadmeddiche2004201@gmail.com>
2026-06-12 14:38:19 +0100

    Initial commit

---
+++ src/main/java/com/saadMeddiche/findById_anti_pattern/FindByIdAntiPatternApplication.java
@@ -0,0 +1,13 @@
+package com.saadMeddiche.findById_anti_pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindByIdAntiPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindByIdAntiPatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saadMeddiche/findById_anti_pattern/FindByIdAntiPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saadMeddiche.findById_anti_pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindByIdAntiPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
