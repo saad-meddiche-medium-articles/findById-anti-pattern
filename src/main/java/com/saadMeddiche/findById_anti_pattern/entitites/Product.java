402594277b770f32f45955fa5ca58abb28a6c52e
SaadMeddiche <saadmeddiche2004201@gmail.com>
2026-06-12 14:42:35 +0100

    feat: create product table

---
+++ src/main/java/com/saadMeddiche/findById_anti_pattern/entitites/Product.java
@@ -0,0 +1,31 @@
+package com.saadMeddiche.findById_anti_pattern.entitites;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Getter @Setter
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "product_sequence_generator")
+    @SequenceGenerator(name = "product_sequence_generator", sequenceName = "product_sequence")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, length = 5000)
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false, unique = true)
+    private UUID serial;
+
+}
